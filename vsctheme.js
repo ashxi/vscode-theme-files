215751e06c8d2f9f97b3040dda6bc6fed27ce4a0
ashe <rosascript@gmail.com>
2022-04-16 19:35:00 -0400

    add notice

---
+++ vsctheme.js
@@ -0,0 +1,16 @@
+/******************************************************************************************************************************************************************************************
+*   this source code is copyright (c) under MIT license by @ashxi on Github.
+*   the github repository for this code is located at `https://github.com/ashxi/vscode-theme-files`.
+*   all copies of this code must retain this copyright/description notice.
+*   a markdown-style copy of the MIT license can be found in the LICENSE file, or printed to stdout using `vsctheme.js --license`.
+*
+*   this source code takes a single file as input, and outputs a single file in the VSCode theme format, with the filename of `(input filename without any extensions).json`.
+*   the output file is written to the current working directory (can be found by running `pwd` on *nix operating systems).
+*   a list of all options can be generated using `vsctheme.js --help`.
+*
+*   if you are contributing to this project, please follow the git convention described at `https://github.com/ashxi/git-convention/`.
+*
+*   any comments that start with `xxx`, `fixme`, or `todo` are considered programmer notes, and are to be used for future development, or a guide on how to use that section of the code.
+*   always run `(<yarn> or <npm run>) precommit` before committing, to ensure that your added code is properly formatted & linted.
+*   this program is written in `Deno`, and intended to be written in VSCode with the official `Deno` extension, provided by `https://deno.land` themselves.
+******************************************************************************************************************************************************************************************/
